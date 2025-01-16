1cd8ea18af26baa142832e2e09b72eb806ca83ce
Alvinda_akbar10 <alvindaakbar@gmail.com>
2025-01-16 13:12:00 +0700

    Menambahkan fitur perkalian

--- fungsi.js
+++ fungsi.js
@@ -8,6 +8,12 @@ const sub = (a, b) => {
     return a - b;
 }
 
+const multiply = (a, b) => {
+    return a * b;
+}
+
 console.log(add(3,5));
 
 console.log(sub(4,2));
+
+console.log(multiply(2,2));
